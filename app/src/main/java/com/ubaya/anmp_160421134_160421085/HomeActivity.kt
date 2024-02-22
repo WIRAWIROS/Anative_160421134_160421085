c8b19b0f9f81638f6df5d4410ba3e277c6995fde
WIRA <s160421134@student.ubaya.ac.id>
2024-02-22 15:08:00 +0700

    Home Activity Add

---
+++ app/src/main/java/com/ubaya/anmp_160421134_160421085/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.ubaya.anmp_160421134_160421085
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
