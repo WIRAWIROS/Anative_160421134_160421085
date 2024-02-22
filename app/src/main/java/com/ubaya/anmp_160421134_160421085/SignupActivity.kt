7ca06616e35c64eceaa5061410f2c385af190680
WIRA <s160421134@student.ubaya.ac.id>
2024-02-22 14:57:03 +0700

    Signup Comment Add

--- app/src/main/java/com/ubaya/anmp_160421134_160421085/SignupActivity.kt
+++ app/src/main/java/com/ubaya/anmp_160421134_160421085/SignupActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SignupActivity : AppCompatActivity() {
+    ///dsfsdf
+    ///dsfssa
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
